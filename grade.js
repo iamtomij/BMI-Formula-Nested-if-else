d7c161c778f218946d8c7db8693a3da58f22f2e0
iamtomij <helloiamtomij@gmail.com>
2026-02-06 20:48:53 +0600

    grade if-else practice

---
+++ grade.js
@@ -0,0 +1,15 @@
+let score = 95;
+
+if (score >= 90 && score <= 100) {
+  console.log("Grade A");
+} else if (score >= 80 && score <= 89) {
+  console.log("Grade B");
+} else if (score >= 70 && score <= 79) {
+  console.log("Grade C");
+} else if (score >= 60 && score <= 69) {
+  console.log("Grade D");
+} else if (score <= 50 && score >= 59) {
+  console.log("Grade E");
+} else if (score <= 40 && score >= 49) {
+  console.log("Grade F");
+}
